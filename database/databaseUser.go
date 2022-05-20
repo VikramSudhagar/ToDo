a818cc33641c4f4477e5db01f212b1bbf445b7cb
VikramSudhagar <vsudhaga@uwaterloo.ca>
2022-05-20 15:01:52 -0700

    feat: argon2id hashing for passwords in db

--- database/databaseUser.go
+++ database/databaseUser.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"todo/models"
 
+	"github.com/alexedwards/argon2id"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -25,10 +26,24 @@ func UserSetUp() {
 func VerifyUser(email string, password string) (models.User, error) {
 	//Verify whether the email and password exist
 	db := UserDBConn
-	var user models.User = models.User{}
-	err := db.Where("email = ? AND password = ?", email, password).Find(&user).Error
-	if errors.Is(err, gorm.ErrRecordNotFound) {
+	user := models.User{}
+	dbUser := models.User{}
+	//Retrieve the user information from the database based of the email information
+	if err := db.Where("email = ?", email).First(&dbUser).Error; err != nil {
+		//The user does not exist
 		return user, err
+	}
+	match, e := argon2id.ComparePasswordAndHash(password, dbUser.Password)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	if match {
+		log.Println("User credentials are valid")
+		user = dbUser
+	} else {
+		log.Println("The user credentials are invalid")
+		return user, gorm.ErrRecordNotFound
 	}
 	return user, nil
 }
@@ -46,6 +61,12 @@ func GetUser(id string) (error, models.User) {
 func AddUser(user models.User) error {
 	db := UserDBConn
 	log.Println("The user value is:", user)
+	//Hash the password with argon2id
+	hash, err := argon2id.CreateHash(user.Password, argon2id.DefaultParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	user.Password = hash
 	if err := db.Create(&user); err != nil {
 		return err.Error
 	}
